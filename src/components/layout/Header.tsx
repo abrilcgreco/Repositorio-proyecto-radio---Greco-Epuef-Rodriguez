77e9183d39753450bd85a18242776be5468b1be6
Jazzz <jazminrod724@gmail.com>
2026-09-23 00:52:05 -0300

    Header y paginas

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,22 @@
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css';
+import logo from "../../assets/logo.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link to="/" className="header-logo" aria-label="Inicio">
+          <img src={logo} alt="Logo de la radio" />
+        </Link>
+        <nav className="header-nav">
+          <ul className="header-links">
+            <li><NavLink to="/" end>Inicio</NavLink></li>
+            <li><NavLink to="/Reproductor">Reproductor</NavLink></li>
+            <li><NavLink to="/Novedades">Novedades</NavLink></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
